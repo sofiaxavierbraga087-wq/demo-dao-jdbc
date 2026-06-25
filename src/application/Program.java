236465be5c463e12d02ba9ca675d48f0b721eeef
Sofia Xavier <sofiaxavierbraga087@gmail.com>
2026-06-25 19:27:59 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,21 +17,11 @@ public class Program {
 		
 		SellerDao sellerDao=DaoFactory.createSellerDao();
 		
-		System.out.println("==TEST 1:seller findById==");
-		Seller seller= sellerDao.findById(3);
+		Seller seller=sellerDao.findById(3);
 		
-		System.out.println(seller);
+		seller=sellerDao.findById(1);
+		seller.setName("Martha");
+		sellerDao.update(seller);
 		
-		List<Seller>list=new ArrayList<>();
-		
-		list=sellerDao.findAll();
-		for(Seller sel:list) {
-			System.out.println(sel);
-		}
-		
-		Seller newSeller= new Seller(null, "Greg", "greg@gmail.com", new Date(0), 4000.0, obj);
-	
-		sellerDao.insert(newSeller);
-		System.out.println(newSeller.getId());
 	}
 }
